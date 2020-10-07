bdd7ee905258bb40ac7ead69d409e443fb7bea3f
ugocottin <ugo.cottion@gmail.com>
2020-10-07 09:17:47 +0200

    adding codingStyle

--- src/fr/ul/acl/model/Pacman.java
+++ src/fr/ul/acl/model/Pacman.java
@@ -1,6 +1,16 @@
+/*
+ * Pacman.java
+ * ACL-2020-pacmasque
+ *
+ * Created by ugocottin on 7/10/2020.
+ * Copyright © 2020 ugocottin. All rights reserved.
+ */
+
 package fr.ul.acl.model;
+
+
 
 public class Pacman {
 
-    public Pacman(){}
+	public Pacman(){}
 }
