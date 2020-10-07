37882aab279cb9c27d7627c03c914d6991f0615b
ugocottin <ugo.cottion@gmail.com>
2020-10-07 09:04:21 +0200

    update copyright

--- src/fr/ul/acl/start/Main.java
+++ src/fr/ul/acl/start/Main.java
@@ -1,3 +1,11 @@
+/*
+ * Main.java
+ * ACL-2020-pacmasque
+ *
+ * Created by ugocottin on 7/10/2020.
+ * Copyright © 2020 ugocottin. All rights reserved.
+ */
+
 package fr.ul.acl.start;
 
 public class Main {
