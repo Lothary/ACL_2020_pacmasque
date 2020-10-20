4c5aa4f03cda60a38b8483bf382b8cdeae5a7c86
Ugo Cottin <ugo.cottin@gmail.com>
2020-10-20 20:51:35 +0200

    ajout du LabyrinthLoader

---
+++ core/src/fr/ul/pacmasque/util/LabyrinthLoader.java
@@ -0,0 +1,29 @@
+/*
+ * LabyrinthLoader.java
+ * ACL_2020_pacmasque
+ *
+ * Created by ugocottin on 20/10/2020.
+ * Copyright © 2020 ugocottin. All rights reserved.
+ */
+
+package fr.ul.pacmasque.util;
+
+import com.badlogic.gdx.Gdx;
+
+import java.io.File;
+
+/**
+ * Le but de cette classe est de charger un fichier de
+ * configuration de labyrinthes, et de sélectionner le
+ * builder adapté
+ */
+public class LabyrinthLoader {
+
+	private LabyrinthLoader() { }
+
+	public static void loadFile(File file) {
+
+	}
+
+	public static void builderForFile() { }
+}
