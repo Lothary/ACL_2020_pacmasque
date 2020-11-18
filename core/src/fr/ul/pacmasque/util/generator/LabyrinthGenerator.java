f61c49fc5f9a8b36f594b1962e99f453f2f3b119
Ugo Cottin <ugo.cottin@gmail.com>
2020-11-18 18:11:12 +0100

    ADD interface de création de labyrinthe LabyrinthGenerator

---
+++ core/src/fr/ul/pacmasque/util/generator/LabyrinthGenerator.java
@@ -0,0 +1,27 @@
+/*
+ * LabyrinthGenerator.java
+ * ACL_2020_pacmasque
+ *
+ * Created by ugocottin on 18/11/2020.
+ * Copyright © 2020 ugocottin. All rights reserved.
+ */
+
+package fr.ul.pacmasque.util.generator;
+
+import fr.ul.pacmasque.model.Labyrinth;
+
+/**
+ * Générateur de labyrinthe
+ */
+public interface LabyrinthGenerator {
+
+
+	/**
+	 * Génère un labyrinthe de taille donnée
+	 * @param width largeur du labyrinthe
+	 * @param height hauteur du labyrinthe
+	 * @return un labyrinthe
+	 */
+	Labyrinth generate(int width, int height);
+
+}
