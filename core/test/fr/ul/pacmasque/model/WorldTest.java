88fcf09a40d5c113d1fc36408628be32a202b22c
Lothary <nicolas54250@hotmail.fr>
2020-12-02 10:26:16 +0100

    +Création du World avec Kruskal dans les classes de tests

--- core/test/fr/ul/pacmasque/model/WorldTest.java
+++ core/test/fr/ul/pacmasque/model/WorldTest.java
@@ -9,12 +9,18 @@
 package fr.ul.pacmasque.model;
 
 import fr.ul.pacmasque.PacmasqueTest;
+import fr.ul.pacmasque.util.generator.Generators;
+import fr.ul.pacmasque.util.generator.LabyrinthGenerator;
+import fr.ul.pacmasque.util.generator.LabyrinthGeneratorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
+import java.util.function.Supplier;
 
+import static com.badlogic.gdx.math.MathUtils.random;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class WorldTest extends PacmasqueTest {
 
@@ -23,25 +29,38 @@ class WorldTest extends PacmasqueTest {
 	private int height;
 
 	private World world;
+	String seed = "" + System.currentTimeMillis();
 
 	@BeforeEach
-	void setUp() {
-		this.width = 4;
-		this.height = 4;
+	void setUp() throws LabyrinthGeneratorException {
+		this.width = random.nextInt(100);
+		this.height = random.nextInt(100);
 
-		Labyrinth labyrinth = new Labyrinth(width, height);
-		this.world = new World(labyrinth, "test");
+		if(this.width < 3)
+			this.width += 5;
+		if(this.height < 3)
+			this.height += 5;
+		if(this.width % 2 == 0)
+			this.width -= 1;
+		if(this.height % 2 == 0)
+			this.height -= 1;
+
+		Supplier<LabyrinthGenerator> generatorFactory = Generators.shared().getGeneratorFactory("Kruskal");
+		LabyrinthGenerator generator = generatorFactory.get();
+		Labyrinth labyrinth = generator.generate(System.currentTimeMillis(), (int) width, (int) height);
+
+		this.world = new World(labyrinth, "New World");
 	}
 
-	//@Test
-	//void getWidth() {
-	//	assertEquals(this.width, this.world.getWidth());
-	//}
+	@Test
+	void getWidth() {
+		assertEquals(this.width, this.world.getWidth());
+	}
 
-	//@Test
-	//void getHeight() {
-	//	assertEquals(this.height, this.world.getHeight());
-	//}
+	@Test
+	void getHeight() {
+		assertEquals(this.height, this.world.getHeight());
+	}
 
 	//@Test
 	//void movePlayer() {
