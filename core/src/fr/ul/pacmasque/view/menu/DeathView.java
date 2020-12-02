7e5275d5ebfe7836df85134590033f89a3019cce
Ugo Cottin <ugo.cottin@gmail.com>
2020-12-02 09:38:05 +0100

    ajout de la vue de mort

--- core/src/fr/ul/pacmasque/view/menu/DeathView.java
+++ core/src/fr/ul/pacmasque/view/menu/DeathView.java
@@ -1,14 +1,13 @@
 /*
- * ErrorView.java
+ * DeathView.java
  * ACL_2020_pacmasque
  *
- * Created by ugocottin on 25/11/2020.
+ * Created by ugocottin on 2/12/2020.
  * Copyright © 2020 ugocottin. All rights reserved.
  */
 
 package fr.ul.pacmasque.view.menu;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -16,13 +15,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import fr.ul.pacmasque.model.World;
 import fr.ul.pacmasque.view.hierarchy.OverlayView;
-import fr.ul.pacmasque.view.hierarchy.StageView;
 import org.jetbrains.annotations.NotNull;
 
-public class ErrorView extends OverlayView {
+public class DeathView extends OverlayView {
 
-	private final String message;
+	private World world;
 
 	/**
 	 * Crée une vue munie d'une scène
@@ -31,15 +30,13 @@ public class ErrorView extends OverlayView {
 	 * @param viewportHeight  la hauteur du viewport
 	 * @param skin            le skin utilisé par la scène
 	 */
-	public ErrorView(float viewportWidth, float viewportHeight, @NotNull Skin skin, String message) {
-
+	public DeathView(float viewportWidth, float viewportHeight, @NotNull Skin skin, World world) {
 		super(viewportWidth, viewportHeight, null, skin);
-		this.message = message;
+		this.world = world;
 	}
 
 	@Override
 	public void build(@NotNull Stage stage, boolean debug) {
-
 		Skin skin = this.getSkin();
 
 		Table table = new Table();
@@ -48,22 +45,32 @@ public class ErrorView extends OverlayView {
 		table.setDebug(debug);
 		table.center();
 
-		Label titleLabel = new Label("Une erreur est survenue", skin);
-		Label messageLabel = new Label(this.message, skin);
+		Label titleLabel = new Label("Game Over", skin);
 
-		TextButton okButton = new TextButton("Ok", skin);
-		okButton.addListener(new ClickListener() {
+		TextButton restartButton = new TextButton("Restart", skin);
+		restartButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
+				world.restart();
+				dismiss();
+			}
+		});
+
+		TextButton exitButton = new TextButton("Exit", skin);
+		exitButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				dismiss();
+				dismiss();
 				dismiss();
 			}
 		});
 
 		table.add(titleLabel).width(400).fillX();
 		table.row();
-		table.add(messageLabel).width(400).fillX();
+		table.add(restartButton).width(400).fillX();
 		table.row();
-		table.add(okButton).width(400).fillX();
+		table.add(exitButton).width(400).fillX();
 		table.row();
 
 		stage.addActor(table);
